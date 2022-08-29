a08da5db14c0573ebd39808d0d1af387aca85f4b
Acaciano Neves Souza <acaciano.neves@gmail.com>
2022-08-29 07:30:26 -0300

    subindo modal

--- ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/wwwroot/js/site.js
@@ -6,6 +6,20 @@
 $(document).ready(function () {
     getDatatable('#table-contatos');
     getDatatable('#table-usuarios');
+
+    $('.btn-total-contatos').click(function () {
+        var usuarioId = $(this).attr('usuario-id');
+
+        $.ajax({
+            type: 'GET',
+            url: '/Usuario/ListarContatosPorUsuarioId/' + usuarioId,
+            success: function (result) {
+                $("#listaContatosUsuario").html(result);
+                $('#modalContatosUsuario').modal();
+                getDatatable('#table-contatos-usuario');
+            }
+        });
+    });
 })
 
 function getDatatable(id) {
